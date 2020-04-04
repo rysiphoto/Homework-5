f5538469b7ee8af43abbcd0dc10945dcc7dc4f90
Ryan Siverson <rcsskier@mac.com>
2020-04-03 19:54:59 -0600

    got the schedule to light correctly to reflect time change

--- style.js
+++ style.js
@@ -3,10 +3,6 @@ $(document).ready(function () {
 
     $("#currentDay").append(moment().format('dddd'));
 
-    var divVal = $(".hour").attr('value');
-    console.log(divVal);
-
-
 
     $("div.time-past").each(function () {
 
@@ -22,56 +18,4 @@ $(document).ready(function () {
         }
     });
 
-    console.log("outside for loop");
-    // addclass
-
-
-
-
-    $(this).on('unload', function () {
-        saveSettings();
-        loadSettings();
-    });
-
-    function loadSettings() {
-        $('#nine').val(JSON.localStorage.nine);
-        $('#ten').val(localStorage.ten);
-        $('#eleven').val(localStorage.eleven);
-        $('noon').val(localStorage.noon);
-        $("one").val(localStorage.one);
-        $("two").val(localStorage.two);
-        $("three").val(localStorage.three);
-        $("four").val(localStorage.four);
-        $("five").val(localStorage.five);
-    }
-
-    function saveSettings() {
-        var nine = document.getElementById("nine").value;
-        var ten = document.getElementById("ten").value;
-        var eleven = document.getElementById("eleven").value;
-        var noon = document.getElementById("noon").value;
-        var one = document.getElementById("one").value;
-        var two = document.getElementById("two").value;
-        var three = document.getElementById("three").value;
-        var four = document.getElementById("four").value;
-        var five = document.getElementById("five").value;
-    }
-    function saveSettings() {
-        localStorage.nine = $('#nine').val();
-        localStorage.ten = $('#ten').val();
-        localStorage.eleven = $('#eleven').val();
-        localStorage.noon = $('#noon').val();
-        localStorage.one = $('#one').val();
-        localStorage.two = $('#two').val();
-        localStorage.three = $('#three').val();
-        localStorage.four = $('#four').val();
-        localStorage.five = $('#five').val();
-
-    }
-
-    $("#saveBtn").on("click", function () {
-        event.preventDefault();
-
-
-    });
 });
